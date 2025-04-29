381a15646496fa7f3d4b677211326ade9b9c8ddc
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-04-29 21:40:11 +0700

    feat: setup project and add TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="flex justify-center min-h-screen items-center font-bold text-2xl">
+        Setup Tailwindcss
+      </h1>
+    </div>
+  )
+}
+
+export default App
